2aa608ef787a9a8e19c41d850825d5ace64d9095
willuwontu <willuwontu3@gmail.com>
2022-03-08 13:17:26 -0800

    Initial commit

---
+++ ShopUI/Utils/Purchasable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ItemShops.Utils
+{
+    abstract class Purchasable
+    {
+        public abstract string Name { get; }
+        public abstract Dictionary<string, int> Cost { get; }
+        public abstract PurchaseLimit PurchaseLimit { get; }
+        public abstract GameObject CreateItem();
+        public abstract void OnPurchase(Player player, Purchasable item);
+        public abstract List<Tag> Tags { get; }
+
+        internal GameObject _gameObject = null;
+
+        public GameObject GameObject { get { return _gameObject; } }
+
+        internal int _timesPurchased = 0;
+        internal Dictionary<Player, int> _timesPlayerPurchased = new Dictionary<Player, int>();
+        
+        public int TimesPurchased { get { return _timesPurchased; } }
+        public Dictionary<Player, int> TimesPlayerPurchased { get { return _timesPlayerPurchased; } }
+
+        public void ResetPurchases()
+        {
+            _timesPurchased = 0;
+            _timesPlayerPurchased.Clear();
+        }
+    }
+}
---
+++ ShopUI/Utils/Shop.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using Photon.Pun;
+using UnboundLib.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace ItemShops.Utils
+{
+    class Shop : MonoBehaviour
+    {
+        string name;
+
+        List<Purchasable> _items = new List<Purchasable>();
+
+        ReadOnlyCollection<Purchasable> ShopItems
+        {
+            get
+            {
+                return new ReadOnlyCollection<Purchasable>(_items);
+            }
+        }
+
+        public void AddItem(Purchasable item)
+        {
+            _items.Add(item);
+        }
+
+        public void AddItems(Purchasable[] items)
+        {
+            _items.AddRange(items);
+        }
+
+        public void RemoveItem(Purchasable item)
+        {
+            _items.Remove(item);
+        }
+
+        public void ClearShop()
+        {
+            _items.Clear();
+        }
+
+        public void DisplayedColumns()
+        {
+            bleh;
+        }
+
+        public void ItemSizes()
+        {
+            bleh;
+        }
+
+        private void ItemsChanged(object sender, NotifyCollectionChangedEventArgs args)
+        {
+            Purchasable[] changedItems;
+            switch (args.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    changedItems = ((IList<Purchasable>)args.NewItems).ToArray();
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    changedItems = ((IList<Purchasable>)args.OldItems).ToArray();
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Awake()
+        {
+            ShopManager.instance._shops.Add(this);
+            OnAwake();
+        }
+
+        public virtual void OnAwake()
+        {
+
+        }
+
+        private void OnDestroy()
+        {
+            ShopManager.instance._shops.Remove(this);
+            OnOnDestroy();
+        }
+
+        public virtual void OnOnDestroy()
+        {
+
+        }
+    }
+}
